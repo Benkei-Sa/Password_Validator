df7023cf648604428693c62729d32e182c2602ba
Benkei-Sa <yaroslav.buzlov@gmail.com>
2022-12-01 11:24:48 +0300

    ready

--- Password_Validator.go
+++ Password_Validator.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"unicode"
+	"unicode/utf8"
 )
 
 // validator проверяет строку на соответствие некоторому условию
@@ -11,31 +13,82 @@ type validator func(s string) bool
 // digits возвращает true, если s содержит хотя бы одну цифру
 // согласно unicode.IsDigit(), иначе false
 func digits(s string) bool {
-	// ...
+	var result bool
+	var a int
+	for _, n := range s {
+		if unicode.IsDigit(n) == true {
+			a++
+			break
+		}
+	}
+	if a == 1 {
+		result = true
+	} else {
+		result = false
+	}
+	return result
 }
 
 // letters возвращает true, если s содержит хотя бы одну букву
 // согласно unicode.IsLetter(), иначе false
 func letters(s string) bool {
-	// ...
+	var result bool
+	var a int
+	for _, n := range s {
+		if unicode.IsLetter(n) == true {
+			a++
+			break
+		}
+	}
+	if a == 1 {
+		result = true
+	} else {
+		result = false
+	}
+	return result
 }
 
 // minlen возвращает валидатор, который проверяет, что длина
 // строки согласно utf8.RuneCountInString() - не меньше указанной
 func minlen(length int) validator {
-	// ...
+	var validator = func(s string) bool {
+		var result bool
+		if utf8.RuneCountInString(s) == length {
+			result = true
+		} else {
+			result = false
+		}
+		return result
+	}
+	return validator
 }
 
 // and возвращает валидатор, который проверяет, что все
 // переданные ему валидаторы вернули true
 func and(funcs ...validator) validator {
-	// ...
+	return func(s string) bool {
+		for _, f := range funcs {
+			if f(s) == false {
+				return false
+				break
+			}
+		}
+		return true
+	}
 }
 
 // or возвращает валидатор, который проверяет, что хотя бы один
 // переданный ему валидатор вернул true
 func or(funcs ...validator) validator {
-	// ...
+	return func(s string) bool {
+		for _, f := range funcs {
+			if f(s) == true {
+				return true
+				break
+			}
+		}
+		return false
+	}
 }
 
 // password содержит строку со значением пароля и валидатор
@@ -47,7 +100,8 @@ type password struct {
 // isValid() проверяет, что пароль корректный, согласно
 // заданному для пароля валидатору
 func (p *password) isValid() bool {
-	// ...
+	result := p.validator(p.value)
+	return result
 }
 
 // ┌─────────────────────────────────┐
